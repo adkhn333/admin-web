e1d8cb28f0bf65fce6b8af4866d67878e2581a3f
Deepank Agarwal <deepank411@gmail.com>
2016-06-25 13:10:09 +0530

    admin details manager bug resolved

--- admin/admin-detail.controller.js
+++ admin/admin-detail.controller.js
@@ -1,9 +1,8 @@
 app.controller("adminDetailCtrl", ['$scope', '$stateParams', '$http', '$location', '$mdSidenav', '$localStorage', '$mdToast', '$mdDialog', 'AdminService', function($scope, $stateParams, $http, $location, $mdSidenav, $localStorage, $mdToast, $mdDialog, AdminService){
 
    $scope.emailid = $localStorage.currentUser.email;
-
    $scope.id = $stateParams.admin_id;
-   console.log($scope.id);
+   // console.log($scope.id);
    $scope.admin = [];
    // console.log($scope.id);
 
@@ -11,45 +10,38 @@ app.controller("adminDetailCtrl", ['$scope', '$stateParams', '$http', '$location
    var ref = db.ref().child("admins").child($scope.id);
    ref.once("value", function(snapshot){
       $scope.admin = snapshot.val();
-      console.log($scope.admin);
-      old_marketing = $scope.admin.marketing_manager;
-      old_dataValidation = $scope.admin.validation_manager;
-      console.log(old_marketing, old_dataValidation);
+      // console.log($scope.admin);
    }, function(errorObject){
-      console.log(errorObject);
+      // console.log(errorObject);
    });
 
    // TODO: check whether getting distinct designations from server is better or directly
    list_service = AdminService.getAllAdminDetailsRequest();
-   console.log(list_service);
+   // console.log(list_service);
    list_service.then(function(alladminlist){
       $scope.alladminlist = alladminlist;
-      console.log(alladminlist);
+      // console.log(alladminlist);
       $scope.allDesignationsList = [];
       for(var i=0; i<alladminlist.length; i++){
-         console.log(alladminlist[i].designation);
+         // console.log(alladminlist[i].designation);
          if($scope.allDesignationsList.indexOf(alladminlist[i].designation) == -1 && alladminlist[i].designation != undefined){
             $scope.allDesignationsList.push(alladminlist[i].designation);
          }
       }
-      console.log($scope.allDesignationsList);
+      // console.log($scope.allDesignationsList);
    });
 
+   var old_marketing, old_validation, new_marketing, new_validation;
+
+   $scope.$watch('admin.marketing_manager', function(newValue, oldValue){
+      new_marketing = newValue;
+      old_marketing = oldValue;
+   });
+   $scope.$watch('admin.validation_manager', function(newValue, oldValue){
+      new_validation = newValue;
+      old_validation = oldValue;
+   });
    $scope.enterAdminPostionDetails = function(){
-      console.log(old_marketing, old_dataValidation);
-
-      if(old_marketing != undefined){
-         var old_marketing_updates = {};
-         old_marketing_updates['/admins/' + old_marketing + '/reportedBy/marketing/' + $scope.id] = false;
-         db.ref().update(old_marketing_updates);
-      }
-
-      if(old_dataValidation != undefined){
-         var old_data_validation_updates = {};
-         old_data_validation_updates['/admins/' + old_dataValidation + '/reportedBy/dataValidation/' + $scope.id] = false;
-         db.ref().update(old_data_validation_updates);
-      }
-
       var posObject = {
          admin_id: $scope.id,
          team: $scope.admin.team,
@@ -61,45 +53,53 @@ app.controller("adminDetailCtrl", ['$scope', '$stateParams', '$http', '$location
          tracking_id: $localStorage.currentUser.uid
       }
       console.log(posObject);
-      // addAdminPositionDetails(posObject);
       var ref = db.ref().child("admins").child($scope.id);
       ref.update(posObject);
 
-      var reporting = {
-         admin_id: $scope.id
-      };
-
       var marketing_updates = {};
-      marketing_updates['/admins/' + $scope.admin.marketing_manager + '/reportedBy/marketing/' + $scope.id] = true;
+      marketing_updates['/admins/' + $scope.admin.marketing_manager + '/managerFor/marketing/' + $scope.id] = true;
       db.ref().update(marketing_updates);
 
       var data_validation_updates = {};
-      data_validation_updates['/admins/' + $scope.admin.validation_manager + '/reportedBy/dataValidation/' + $scope.id] = true;
+      data_validation_updates['/admins/' + $scope.admin.validation_manager + '/managerFor/dataValidation/' + $scope.id] = true;
       db.ref().update(data_validation_updates);
+
+      if(old_marketing != undefined){
+         console.log(old_marketing);
+         var old_marketing_updates = {};
+         old_marketing_updates['/admins/' + old_marketing + '/managerFor/marketing/' + $scope.id] = false;
+         db.ref().update(old_marketing_updates);
+      }
+
+      if(old_validation != undefined){
+         var old_data_validation_updates = {};
+         old_data_validation_updates['/admins/' + old_validation + '/managerFor/dataValidation/' + $scope.id] = false;
+         db.ref().update(old_data_validation_updates);
+      }
    };
 
-   function getAllCities(){
-      $http({
-         method: 'GET',
-         url: 'http://139.162.44.67/getLocationDetails'
-      })
-      .then(function successCallback(response){
-         if(response.data.StatusCode == '200'){
-            $scope.allCities = response.data.Items;
-            // console.log($scope.allCities);
-         }
-         else{
-            $mdToast.show(
-               $mdToast.simple()
-                 .textContent(data.Message)
-                 .hideDelay(3000)
-            );
-         }
-      }, function errorCallback(response){
-         console.log("Error in callback get cities");
-      });
-   }
-
-   getAllCities();
+   // function getAllCities(){
+   //    $http({
+   //       method: 'GET',
+   //       url: 'http://139.162.44.67/getLocationDetails'
+   //    })
+   //    .then(function successCallback(response){
+   //       if(response.data.StatusCode == '200'){
+   //          $scope.allCities = response.data.Items;
+   //          // console.log($scope.allCities);
+   //       }
+   //       else{
+   //          $mdToast.show(
+   //             $mdToast.simple()
+   //               .textContent(data.Message)
+   //               .hideDelay(3000)
+   //          );
+   //       }
+   //    }, function errorCallback(response){
+   //       console.log("Error in callback get cities");
+   //    });
+   // }
+   //
+   // getAllCities();
 
 }]);
